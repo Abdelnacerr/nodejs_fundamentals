115c9441dc7cbe0b25c2fde35da3f14806216338
Mohamed Abdinasir <abdelnacerr@gmail.com>
2021-12-27 23:00:05 +1100

    get/post/delete/update

--- server.js
+++ server.js
@@ -25,11 +25,48 @@ app.get('/api/people', (req, res) => {
 })
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-app.post('/login', (req, res) => {
-	let person = req.body
-	people.push(person)
-	res.status(201).json({ success: true, data: person })
+app.post('api/people', (req, res) => {
+	const { name } = req.body
+	if (!name) {
+		return res
+			.status(400)
+			.json({ success: false, msg: 'please provide name value' })
+	}
+	people.push(name)
+	res.status(201).send({ success: true, person: name })
+})
+
+app.post('api/postman/people', (req, res) => {
+	const { name } = req.body
+	if (!name) {
+		return res
+			.status(400)
+			.json({ success: false, msg: 'please provide name value' })
+	}
+	people.push(name)
+	res.status(201).send({ success: true, data: [...people, name] })
+	console.log(id, name)
+})
+
+app.put('api/people:id', (req, res) => {
+	const { id } = req.params
+	const { name } = req.body
+	console.log(id, name)
+})
+
+app.delete('api/people:id', (req, res) => {
+	const person = people.find((person) => person.id === Number(req.params.id))
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `no person with id ${req.params.id}` })
+	}
+	const newPeople = people.filter(
+		(person) => person.id !== Number(req.params.id)
+	)
+	return res.status(200).json({ success: true, data: newPeople })
 })
 
 app.listen(3000, () => {
